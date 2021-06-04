373d8fcc669bd3ed5031a77e4e5a3f5f955289ad
Ruben Rådelius <ruben.radelius@gmail.com>
2021-06-04 13:29:54 +0200

    added random number generation

--- random_number/main.cpp
+++ random_number/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int iterations = 1000;
+    int distribution[20]{0};
+    srand(time(0));
+
+    for (int i = 0; i < iterations; i++){
+        distribution[rand() % 20]++;
+    }
+
+    for (int i = 0; i < 20; i++) {
+        std::cout << i + 1 << ": " << distribution[i] << std::endl;
+    }
+
     return 0;
 }
